8450e5aef6cc35facdb6fcd2503579f4f901b0a0
mccameron <cmcdonald@chibi.ubc.ca>
2014-01-29 12:08:33 -0800

    throw GWT in the garbage

--- aspiredb/oldGWTclient/client/HasVariantFilterHandlers.java
+++
@@ -1,8 +0,0 @@
-package ubc.pavlab.aspiredb.client;
-
-import com.google.gwt.event.shared.HandlerRegistration;
-import ubc.pavlab.aspiredb.client.handlers.VariantFilterHandler;
-
-public interface HasVariantFilterHandlers {
-    public HandlerRegistration addVariantFilterHandler( VariantFilterHandler event );
-}
--- aspiredb/oldGWTclient/client/HasVariantSelectionHandlers.java
+++
@@ -1,8 +0,0 @@
-package ubc.pavlab.aspiredb.client;
-
-import com.google.gwt.event.shared.HandlerRegistration;
-import ubc.pavlab.aspiredb.client.handlers.VariantSelectionHandler;
-
-public interface HasVariantSelectionHandlers {
-    public HandlerRegistration addVariantSelectionHandler (VariantSelectionHandler handler);
-}
