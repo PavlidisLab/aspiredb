dda6c2ddb849f5d794ac4b4502637a735327bd59
Patrick <patrick.tan@ubc.ca>
2014-05-14 13:40:01 -0700

    Fixed Bug 4097. Phenotype Enrichment sorting.

--- aspiredb/src/main/webapp/scripts/ASPIREdb/store/PhenotypeEnrichmentStore.js
+++ aspiredb/src/main/webapp/scripts/ASPIREdb/store/PhenotypeEnrichmentStore.js
@@ -1,19 +1,36 @@
-Ext.require([ 'Ext.data.ArrayStore' ]);
+Ext.require( [ 'Ext.data.ArrayStore' ] );
 
-Ext.define('ASPIREdb.store.PhenotypeEnrichmentStore', {
-    extend: 'Ext.data.ArrayStore',
-    alias: 'store.phenotypeEnrichmentStore',
-        
-    fields: [
-             {name: 'name', type: 'string'},
-             {name: 'inGroupPresent',  type: 'string'},
-             {name: 'outGroupPresent', type: 'string'},
-             {name: 'pValue',  type: 'string'},
-             {name: 'corrpValue', type: 'string'}
-         ],
-         
-	
-    storeId: 'phenotypeEnrichments'
-    
-    
-});
+Ext.apply( Ext.data.SortTypes, {
+   asFraction : function(fraction) {
+      var tokens = fraction.split( '/' );
+      if ( tokens.length != 2 ) {
+         return fraction;
+      }
+      return parseInt( tokens[0] ) / parseInt( tokens[1] ) * 1.0;
+   }
+} );
+
+Ext.define( 'ASPIREdb.store.PhenotypeEnrichmentStore', {
+   extend : 'Ext.data.ArrayStore',
+   alias : 'store.phenotypeEnrichmentStore',
+
+   fields : [ {
+      name : 'name',
+      type : 'string'
+   }, {
+      name : 'inGroupPresent',
+      sortType : 'asFraction'
+   }, {
+      name : 'outGroupPresent',
+      sortType : 'asFraction'
+   }, {
+      name : 'pValue',
+      type : 'string'
+   }, {
+      name : 'corrpValue',
+      type : 'string'
+   } ],
+
+   storeId : 'phenotypeEnrichments'
+
+} );
