b4e8b3a0fd549de44a0fbf25b731f3a25c5e81a2
gcharathsandran <gaya@chibi.ubc.ca>
2014-07-18 14:18:05 -0700

    Bug 4168 - filtering based on variant size
    CNV, SNV and indel properties created

---
+++ aspiredb/src/main/java/ubc/pavlab/aspiredb/shared/query/IndelCharacteristicsProperty.java
@@ -0,0 +1,43 @@
+/*
+ * The aspiredb project
+ * 
+ * Copyright (c) 2013 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package ubc.pavlab.aspiredb.shared.query;
+
+import java.util.Arrays;
+
+import org.directwebremoting.annotations.DataTransferObject;
+
+/**
+ * author: gaya date: 07/18/14 Class is concrete because I couldn't get DWR to see it when it is abstract.
+ */
+@DataTransferObject(javascript = "CNVCharacteristicsProperty")
+public class IndelCharacteristicsProperty extends TextProperty {
+
+   
+    /**
+     * generated the serializable id
+     */
+    private static final long serialVersionUID = -6849102681487342527L;
+
+
+        public IndelCharacteristicsProperty() {
+            super( "Indel Characteristics", "Indel", Arrays.asList( "Indel Length","characteristics" ) );
+        }
+   
+    
+}
---
+++ aspiredb/src/main/java/ubc/pavlab/aspiredb/shared/query/SNVCharacteristicsProperty.java
@@ -0,0 +1,49 @@
+/*
+ * The aspiredb project
+ * 
+ * Copyright (c) 2013 University of British Columbia
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package ubc.pavlab.aspiredb.shared.query;
+
+import java.util.Arrays;
+
+import org.directwebremoting.annotations.DataTransferObject;
+
+/**
+ * author: gaya date: 07/18/14 Class is concrete because I couldn't get DWR to see it when it is abstract.
+ */
+@DataTransferObject(javascript = "CNVCharacteristicsProperty")
+public class SNVCharacteristicsProperty extends TextProperty {
+
+   
+    /**
+     * generated the serializable id
+     */
+    private static final long serialVersionUID = -6849102681487342527L;
+
+ /**   public CNVCharacteristicsProperty() {
+        this.displayName = "CNV Characteristics";
+        //this.exampleValues = "Gene Set Names - Examples: <b>geneset1</b>, <b>geneset2</b>, <b>geneset3</b>";
+       // this.operators = Arrays.asList( Operator.IS_IN_SET, Operator.IS_NOT_IN_SET );
+        this.supportsSuggestions = true;
+    }*/
+
+        public SNVCharacteristicsProperty() {
+            super( "SNV Characteristics", "SNV", Arrays.asList( "DB SNP ID", "Observed base", "Reference Base", "characteristics" ) );
+        }
+   
+    
+}
