b545f6916e87ec91a6c840a29e4c339b46eb5d7e
ptan <patrick.tan@ubc.ca>
2014-09-15 13:44:39 -0700

    Bug 4195. Add databases indices to VARIANT table for variantDao.findByUserVariantId() query speedup

--- aspiredb/src/main/resources/sql/init-indices.sql
+++ aspiredb/src/main/resources/sql/init-indices.sql
@@ -4,3 +4,7 @@ alter table PHENOTYPE add INDEX IDX_NAME_VALUES_SUBJECT_FK (NAME,VALUE,SUBJECT_F
 --alter table GENOMIC_LOC add INDEX IDX_CHR_START_END (CHROMOSOME,START,END);
 --alter table GENOMIC_LOC add INDEX IDX_CHR_BIN (CHROMOSOME,BIN);
 --alter table GENOMIC_LOC add INDEX IDX_BIN_CHR (BIN,CHROMOSOME);
+
+alter table VARIANT add key uservariantkey (USERVARIANTID);
+alter table VARIANT add key patientidkey (PATIENT_ID);
+alter table VARIANT add unique key uservariant_patientid_key (USERVARIANTID,PATIENT_ID);
