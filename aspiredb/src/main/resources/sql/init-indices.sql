da3cfbbc7913e0ab05fd85e4e4729fb870b8af8f
ptan <patrick.tan@ubc.ca>
2015-09-03 14:04:07 -0700

    Removed commented lines.

--- aspiredb/src/main/resources/sql/init-indices.sql
+++ aspiredb/src/main/resources/sql/init-indices.sql
@@ -1,9 +1,6 @@
 -- Add some indices. Some of these are very important to performance
 -- $Id$
 alter table PHENOTYPE add INDEX IDX_NAME_VALUES_SUBJECT_FK (NAME,VALUE,SUBJECT_FK);
---alter table GENOMIC_LOC add INDEX IDX_CHR_START_END (CHROMOSOME,START,END);
---alter table GENOMIC_LOC add INDEX IDX_CHR_BIN (CHROMOSOME,BIN);
---alter table GENOMIC_LOC add INDEX IDX_BIN_CHR (BIN,CHROMOSOME);
 
 alter table SUBJECT add key patientidkey (PATIENT_ID);
 
