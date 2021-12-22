28df4f0abe672ee8f01b7e3334355da5a8f3c997
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2021-12-22 12:00:47 -0500

    Update all dependencies
    Use pavlab-starter-parent to manage common dependencies with other
    Pavlidis Lab projects.
    
    Add missing <scm/> and <issueManagement/> entries in pom.xml.
    
    Use Hibernate 3.6.10.Final and remove ehcache dependency since we don't
    use it directly (it is still imported through hibernate-ehcache).
    
    Use gsec 0.0.7 and update context configuration loading accordingly.
    
    Use commons-pool2 and modify data source configurations accordingly.

--- aspiredb/src/test/java/ubc/pavlab/aspiredb/server/BaseSpringContextTest.java
+++ aspiredb/src/test/java/ubc/pavlab/aspiredb/server/BaseSpringContextTest.java
@@ -56,7 +56,7 @@ import gemma.gsec.authentication.UserManager;
  * @version $Id: BaseSpringContextTest.java,v 1.4 2013/06/12 20:18:48 cmcdonald Exp $
  */
 @ContextConfiguration(locations = { "classpath:/test-data-source.xml",
-        "classpath:gemma/gsec/acl/security-bean-baseconfig.xml", "classpath:/application-context.xml",
+        "classpath*:/gemma/gsec/applicationContext-*.xml", "classpath:/application-context.xml",
         "classpath:/applicationContext-security.xml", "classpath*:applicationContext-serviceBeans.xml" })
 public abstract class BaseSpringContextTest extends AbstractJUnit4SpringContextTests implements InitializingBean {
 
